2ef905a01b07f243c06aa69c89fc44df522cb181
scottie-schneider <scott@laclave.co>
2020-01-09 18:27:04 -0500

    airplane passing tests

--- index.js
+++ index.js
@@ -41,6 +41,24 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(food){
+    if(this.stomach.length > 10){
+      return;
+    }
+    this.stomach.push(food);
+    return;
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 
 }
 
